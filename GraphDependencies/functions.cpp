a33d88215e24eb6cc338be21b198db1cc2171492
Artem Arkhipov <Artem-arkhipov-777@yandex.ru>
2026-06-09 15:56:42 +0300

    Реализована функция parseActions

--- GraphDependencies/functions.cpp
+++ GraphDependencies/functions.cpp
@@ -1,26 +1,54 @@
 #include "functions.h"
 
+// Разбор всех строк
+void parseActions(const QStringList& fileContent, QList<Action*>& actions, QSet<Error>& errors)
+{
+    // Обнулить счетчик строк
+    int lineNum = 0;
+    // Для каждой строки
+    for (int i = 0; i < fileContent.size(); i++){
+        // Увеличить счётчик строк
+        lineNum++;
+        // Убрать пробелы по краям
+        const QString trimmedLine = fileContent[i].trimmed();
+        // Если строка непустая - передать в разбор одной строки
+        if (!trimmedLine.isEmpty()) {
+            parseSingleLine(trimmedLine, lineNum, actions, errors);
+        }
+    }
+    // Если количество действий превысило лимит => добавить ошибку
+    if (actions.size() > MAX_ACTIONS) {
+        errors.insert(Error(TooManyActions, 0, 0, ""));
+    }
 
+}
+void parseSingleLine(const QString& trimmedLine, int lineNumber,
+                     QList<Action*>& actions, QSet<Error>& errors)
+{
+    Q_UNUSED(trimmedLine);
+    Q_UNUSED(lineNumber);
+    Q_UNUSED(actions);
+    Q_UNUSED(errors);
+}
 ExprNode* parseExpression(const QString& exprStr,
                           QList<ExprNode*>& sources,
                           QList<ExprNode*>& modified,
                           int lineNumber,
                           QSet<Error>& errors)
 {
+    Q_UNUSED(exprStr);
+    Q_UNUSED(lineNumber);
+    Q_UNUSED(sources);
+    Q_UNUSED(errors);
+    Q_UNUSED(modified);
     return nullptr;
 }
 
 
 DependencyType compareIndices(ExprNode* node1, ExprNode* node2)
 {
+    Q_UNUSED(node1);
+    Q_UNUSED(node2);
     return NoDependency;
 }
 
-void parseActions(const QStringList& fileContent,
-                  QList<Action*>& actions,
-                  QSet<Error>& errors)
-{
-    Q_UNUSED(fileContent);
-    Q_UNUSED(actions);
-    Q_UNUSED(errors);
-}
--- GraphDependencies/functions.h
+++ GraphDependencies/functions.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+#include <QStack>
+#include <QRegularExpression>
 #include <QString>
 #include <QList>
 #include <QSet>
@@ -15,6 +16,18 @@ const int MAX_ACTIONS = 10000;
 const int MAX_VAR_LEN = 255;
 const int MAX_INDEX_DEPTH = 5;
 
+
+/**
+ * @brief Разбирает все строки трассы в список действий.
+ * @param[in]     fileContent  строки файла
+ * @param[out]    actions      список действий
+ * @param[in,out] errors       множество ошибок
+ */
+void parseActions(const QStringList& fileContent, QList<Action*>& actions, QSet<Error>& errors);
+
+void parseSingleLine(const QString& trimmedLine, int lineNumber,
+                     QList<Action*>& actions, QSet<Error>& errors);
+
 ExprNode* parseExpression(const QString& exprStr,
                           QList<ExprNode*>& sources,
                           QList<ExprNode*>& modified,
@@ -23,8 +36,6 @@ ExprNode* parseExpression(const QString& exprStr,
 
 DependencyType compareIndices(ExprNode* node1, ExprNode* node2);
 
-void parseActions(const QStringList& fileContent,
-                  QList<Action*>& actions,
-                  QSet<Error>& errors);
+
 
 #endif // FUNCTIONS_H
